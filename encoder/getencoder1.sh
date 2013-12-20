2639dfa39efef1e01e9e59e91264bbb51fb2e8c4
Mauro Lacy <mauro@lacy.com.ar>
2013-12-20 22:36:58 +0000

    client-server encoder reader.

--- encoder/getencoder1.sh
+++ encoder/getencoder1.sh
@@ -2,19 +2,24 @@
 
 . /etc/dlogger.conf
 
-DURATION=1	# seconds
-INTERVAL=1	# seconds
+DURATION=0	# seconds
+INTERVAL=3	# seconds
+
+PORT=16001
 
 >/tmp/getencoder.log
+COUNTS=0
+OLDCOUNTS=0
 while true
 do
-date | tee -a /tmp/getencoder.log
-ANGLE=`$BASE/encoder/serial/getSerialAsync -d $DURATION 2>>/tmp/getencoder.log`
-SPEED=0 # FIXME
-echo $SPEED | tee -a /tmp/getencoder.log
-mysql -u$US -p$PASS $DB <<EOF
-INSERT INTO \`encoder\` (\`angle\`, \`speed\`, \`duration\`, \`interval\`) VALUES ('$ANGLE', '$SPEED', '$DURATION', '$INTERVAL');
+	date | tee -a /tmp/getencoder.log
+	COUNTS=`nc localhost $PORT 2>>/tmp/getencoder.log`
+	[ -z "$COUNTS" ] && COUNTS=$OLDCOUNTS
+	echo $COUNTS | tee -a /tmp/getencoder.log
+	mysql -u$US -p$PASS $DB <<EOF
+INSERT INTO \`encoder\` (\`counts\`, \`duration\`, \`interval\`) VALUES ('$COUNTS', '$DURATION', '$INTERVAL');
 EOF
-    D=$[INTERVAL - $DURATION]
-    [ $D -gt 0 ] && sleep $D
+	D=$[INTERVAL - $DURATION]
+	[ $D -gt 0 ] && sleep $D
+	OLDCOUNTS=$COUNTS
 done
