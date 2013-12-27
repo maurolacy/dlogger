49a333755c6292423e7506a8e5b8e7d84f277170
Mauro Lacy <mauro@lacy.com.ar>
2013-12-27 20:00:30 +0000

    Fixed driver2 binary.

---
+++ encoder/driver/PIGPIO/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pigpio',
+      version='1.0',
+      author='joan',
+      author_email='joan@abyz.me.uk',
+      maintainer='joan',
+      maintainer_email='joan@abyz.me.uk',
+      url='http://abyz.co.uk/rpi/pigpio/python.html/',
+      description='Raspberry Pi gpio module',
+      long_description='Raspberry Pi Python module for access to the pigpio daemon',
+      download_url='http://abyz.co.uk/rpi/pigpio/pigpio.tar',
+      license='TBD',
+      py_modules=['pigpio']
+     )
