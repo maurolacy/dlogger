604e9cbe6c0f68ca2498d53feb7e7c107c1e8608
Gustavo Santamaria <gustavo@ifa.org.ar>
2015-03-04 12:33:55 -0300

    Add screen install.

--- mysql/dlogger.sh
+++ mysql/dlogger.sh
@@ -5,7 +5,7 @@
 echo -n "Installing mysql server..."
 echo
 sudo apt-get update
-sudo apt-get install mysql-server
+sudo apt-get -y install mysql-server screen
 echo "done."
 echo -n "Creating database '$DB'..."
 echo "CREATE DATABASE $DB;" | mysql -u$US -p$PASS mysql 
