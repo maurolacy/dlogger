2587cd1950fcb009b1b4d7bf14bf9c76a2f74975
Gustavo Santamaria <gustavo@ifa.org.ar>
2015-05-31 21:03:42 +0000

    No lat/lon

--- httpd/pubpart.sh
+++ httpd/pubpart.sh
@@ -3,7 +3,8 @@
 . /etc/dlogger.conf
 LOG=$BASE/logs/`basename $0 .sh`.log
 
-DATE=`date '+%d%m%y'`.txt
-mysql -u$US -p$PASS $DB <<EOF >$DATA/$DATE
-SELECT \`ts\`, \`lat\`, \`lon\`, \`particles\` FROM \`part\`;
+DATE1=`date '+%d%m%y'`.txt
+DATE2=`date '+%Y-%m-%d'`
+mysql -u$US -p$PASS $DB <<EOF >$DATA/$DATE1
+SELECT \`ts\`, \`particles\` FROM \`part\` WHERE \`ts\` > '$DATE2';
 EOF
