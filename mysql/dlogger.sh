dd974cfa532e4e2b66604d1c3e0793424ee5ed37
Mauro Lacy <mauro@lacy.com.ar>
2015-07-23 01:32:01 +0000

    Fixes

--- mysql/dlogger.sh
+++ mysql/dlogger.sh
@@ -5,7 +5,7 @@
 echo -n "Installing mysql server..."
 echo
 #sudo apt-get update
-sudo apt-get -y install mysql-server screen
+sudo apt-get -y install mysql-server python-mysqldb screen
 echo "done."
 echo -n "Setting root password..."
 /usr/bin/mysql -uroot -Dmysql -e"update user set password=password('$PASS') where user='$US'"
--- pressure/getpressure1.py
+++ pressure/getpressure1.py
@@ -69,9 +69,9 @@ if __name__ == '__main__':
         if mode == 'stdout':
             print "Temperature: %.1f °C, Pressure: %.2f hPa" % (T, P)
         elif mode == 'mysql':
-            print "%d %.2f %d" % (time.time(), T, P)
+            print "%d %.1f %.2f" % (time.time(), T, P)
             assert(db_cursor)
-            db_cursor.execute('INSERT INTO `pressure` (`temperature`, `pressure`, `duration`, `interval`) VALUES (%f, %f, %d, %d);' % (T, P, 0, interval))
+            db_cursor.execute('INSERT INTO `pressure` (`temp`, `pressure`, `duration`, `interval`) VALUES (%f, %f, %d, %d);' % (T, P, 0, interval))
         elif mode == 'table':
             print "%d %.1f %.2f" % (time.time(), T, P)
         time.sleep(interval)
