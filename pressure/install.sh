8265db943979c683778f13c18a2a8d92b9942393
Mauro Lacy <mauro@lacy.com.ar>
2015-10-14 00:00:41 +0000

    Get pressure start-stop-daemon

--- pressure/install.sh
+++ pressure/install.sh
@@ -7,16 +7,15 @@ sudo sh -c 'echo "i2c_dev\ni2c_bcm2708" >>/etc/modules'
 WD=`pwd` 
 
 # Instalar apache2
-cd ../httpd
-sudo ./install_publish.sh
+#cd ../httpd
+#sudo ./install_publish.sh
 
 # Configurar script de arranque
 cd /etc/init.d
 sudo ln -s $WD/getpressure
 cd -
 
-# Agregar la línea al archivo /etc/rc.local
-sudo sed -i 's/^exit 0/service getpressure start\n\nexit 0/' /etc/rc.local
+sudo update-rc.d getpressure defaults
+sudo update-rc.d getpressure enable 2 3 4 5
 
-# Reiniciar
-sudo reboot
+sudo service getpressure restart
