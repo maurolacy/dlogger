9ddf02a44ad41c55021442de9200844f0439760e
Mauro Lacy <mauro@lacy.com.ar>
2016-03-23 23:30:12 +0000

    Publish magnetic field magnitude

--- httpd/pubmag.sh
+++ httpd/pubmag.sh
@@ -6,5 +6,5 @@ LOG=$BASE/logs/`basename $0 .sh`.log
 DATE1=`date '+%d%m%y'`.txt
 DATE2=`date '+%Y-%m-%d'`
 mysql -u$US -p$PASS $DB <<EOF >$DATA/mag_$DATE1
-SELECT \`ts\`, \`X\`, \`Y\`, \`Z\` FROM \`mag\` WHERE \`ts\` > '$DATE2';
+SELECT \`ts\`, \`X\`, \`Y\`, \`Z\`, round(sqrt(X*X+Y*Y+Z*Z), 4) AS M FROM \`mag\` WHERE \`ts\` > '$DATE2';
 EOF
