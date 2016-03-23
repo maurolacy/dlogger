ddda4c55ea905d7800d93a1b13435838f89c2d14
Mauro Lacy <mauro@lacy.com.ar>
2016-03-23 20:01:03 +0000

    Fix environment setting

--- mag/env.sh
+++ mag/env.sh
@@ -1,6 +1,10 @@
 :
 
-export QUICK2WIRE_API_HOME=`pwd`/quick2wire-python-api
+[ -z "$BASE" ] && BASE="`pwd`/.."
+
+export QUICK2WIRE_API_HOME=$BASE/mag/quick2wire-python-api
 export PYTHONPATH=$PYTHONPATH:$QUICK2WIRE_API_HOME
+
+#echo "BASE: $BASE, QW: $QUICK2WIRE_API_HOME, PP: $PYTHONPATH" >/tmp/env.$$
 
 #cd $QUICK2WIRE_API_HOME
