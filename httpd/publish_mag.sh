3663dd25d2b2455e11fc916187de2d714e6fa9c2
Mauro Lacy <mauro@lacy.com.ar>
2016-03-24 17:51:50 +0000

    Add magnetic field magnitude publishing

--- httpd/publish_mag.sh
+++ httpd/publish_mag.sh
@@ -10,6 +10,7 @@ SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 
 */15 *    * * *   root   test -x $BASE/httpd/pubmag.sh && $BASE/httpd/pubmag.sh >/dev/null
+*/15 *    * * *   root   test -x $BASE/httpd/plotmag_X_SQRT\(X*X+Y*Y+Z*Z\).sh && /home/pi/work/dlogger/httpd/plotmag_X_SQRT\(X*X+Y*Y+Z*Z\).sh >/dev/null
 */15 *    * * *   root   test -x $BASE/httpd/plotmag_X_Y_Z.sh && $BASE/httpd/plotmag_X_Y_Z.sh >/dev/null
 */15 *    * * *   root   test -x $BASE/httpd/plotmag_Y_Z.sh && $BASE/httpd/plotmag_Y_Z.sh >/dev/null
 */15 *    * * *   root   test -x $BASE/httpd/plotmag_X.sh && $BASE/httpd/plotmag_X.sh >/dev/null
