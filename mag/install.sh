37ea471452e51810fa034d8068066b532b3c8bab
Mauro Lacy <mauro@lacy.com.es>
2022-08-20 21:38:45 +0200

    Update magnetometer install script

--- mag/install.sh
+++ mag/install.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 sudo apt-get update 
-#sudo apt-get -y upgrade 
-sudo apt-get install -y i2c-tools python-smbus install python3 python3-mysql.connector # git
+sudo apt install -y git i2c-tools python-smbus python3 python-pip python-virtualenv python3-setuptools python3-mysql.connector
 
-sudo sh -c 'echo "i2c_dev\ni2c_bcm2708" >>/etc/modules'
+#sudo sh -c 'echo "i2c_dev\ni2c_bcm2708" >>/etc/modules'
 
 WD=`pwd` 
 
-# Get the libraries. Among the libraries we will be needing is "quick2wire" and "i2clibraries" for python from Think-Bowl. Make a folder for your project, lets say the folder's name is "project", type in the following commands. (everything after "#" symbol is just comments for you to read)
-git clone https://github.com/quick2wire/quick2wire-python-api.git
+git clone https://github.com/quick2wire/quick2wire-python-api
 
 export QUICK2WIRE_API_HOME=`pwd`/quick2wire-python-api
 export PYTHONPATH=$PYTHONPATH:$QUICK2WIRE_API_HOME
-
 cd $QUICK2WIRE_API_HOME
 
-git clone https://bitbucket.org/thinkbowl/i2clibraries.git #getting library files containing functions for i2c devices such as HMC5883L, ITG-3205, ADXL345 and LCD
+sudo python3 setup.py install
+
+git clone https://bitbucket.org/thinkbowl/i2clibraries.git
 cd ..
 ln -s quick2wire-python-api/i2clibraries
 
