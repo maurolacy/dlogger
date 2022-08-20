b631c163accfa4b1beadece042fdf441f7b26eec
Mauro Lacy <mauro@lacy.com.es>
2022-08-20 09:27:00 +0200

    Switch to user-space driver for humidity
    Broken kernel driver

--- humidity/gethumidity1.sh
+++ humidity/gethumidity1.sh
@@ -7,35 +7,41 @@ LOG=$BASE/log/`basename $0 .sh`.log
 DURATION=1	 # seconds (Sampling duration. Defined by the reader interface)
 INTERVAL=900 # seconds (Sampling interval. Defined by the reader interface)
 
-DRIVER="dht11"  # Kernel driver
-GPIO=4
-FORMAT=0
+#DRIVER="dht11"  # Kernel driver
+#GPIO=4
+#FORMAT=0
+#DEV="/dev/$DRIVER"
 
-DEV="/dev/$DRIVER"
+USER_DRIVER="./userspace_driver/DHT11" # Userspace driver
 
 # Configure kernel module and device
-insmod $BASE/humidity/kernel_driver/${DRIVER}.ko gpio_pin=$GPIO format=$FORMAT
-mknod $DEV c 80 0 
+#insmod $BASE/humidity/kernel_driver/${DRIVER}.ko gpio_pin=$GPIO format=$FORMAT
+#mknod $DEV c 80 0 
 sleep 1
 
 [ -f "$LOG" ] && mv -f $LOG $LOG.bak
 
+
+R='OK'
 while true
 do
-    L=`cat $DEV`
-	H=`echo $L | cut -d\  -f1`
-	H2=`echo $H | cut -d\. -f1`
-	T=`echo $L | cut -d\  -f2`
-	R=`echo $L | cut -d\  -f4`
-#	[ "$R" = "BAD" ] && continue
-    [ $H2 -gt 100 ] && R="BAD"
-#    [ $T -gt 60 ] && R="BAD"
+  L=$($USER_DRIVER)
+  sleep 1
+  [ "$L" = "." ] && continue
+  H=`echo $L | cut -d\  -f5`
+  H2=`echo $H | cut -d\. -f1`
+  T=`echo $L | cut -d\  -f2`
+  T2=`echo $T | cut -d\. -f1`
+#  R=`echo $L | cut -d\  -f4`
+#[ "$R" = "BAD" ] && continue
+  [ $H2 -gt 100 ] && R="BAD"
+#[ $T -gt 60 ] && R="BAD"
 
-    D=`date '+%s'`
-    echo "$D $H $T $R" | tee -a $LOG
+  D=`date '+%s'`
+  echo "$D $H $T $R" | tee -a $LOG
 
-    mysql -u$US -p$PASS $DB <<EOF
-INSERT INTO \`humidity\` (\`humidity\`, \`temperature\`, \`result\`, \`duration\`, \`interval\`) VALUES ('$H', '$T', '$R', '$DURATION', '$INTERVAL');
+  mysql -u$US -p$PASS $DB <<EOF
+INSERT INTO \`humidity\` (\`humidity\`, \`temperature\`, \`result\`, \`duration\`, \`interval\`) VALUES ('$H2', '$T2', '$R', '$DURATION', '$INTERVAL');
 EOF
-    sleep $INTERVAL
+  sleep $INTERVAL
 done
