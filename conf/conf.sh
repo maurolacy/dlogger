b71cf4ec8023636756d86f8f22dbdfeecf19c499
Mauro Lacy <mauro@lacy.com.es>
2022-08-30 21:58:04 +0200

    Set db password during setup

--- conf/conf.sh
+++ conf/conf.sh
@@ -2,4 +2,14 @@
 
 mkdir -p ../log
 
-sudo cp -f dlogger.conf /etc
+PASS=""
+while [ ${#PASS} -lt 6 ]
+do
+  echo -n "Enter the password you want for the dlogger database (min 6 chars): "
+  read PASS
+done
+
+sed "s/%PASSWORD%/$PASS/" dlogger.conf >/tmp/dlogger.conf.$$
+
+sudo cp -f /tmp/dlogger.conf.$$ /etc
+rm -f /tmp/dlogger.conf.$$
