c0e18cb37bd0b8142fcf37ffa7aac8f9fb678a27
Mauro Lacy <mauro@lacy.com.es>
2022-09-26 09:35:16 +0200

    Remove temp files

--- httpd/dygraphs/pubtemp2.sh
+++ httpd/dygraphs/pubtemp2.sh
@@ -7,8 +7,11 @@ LOG=$BASE/logs/`basename $0 .sh`.log
 cat /var/www/html/temp.txt | sed 's/$/\t/' >/tmp/temp2.$$
 cat /var/www/html/temperature.txt | sed 's/\t/\t\t/' >>/tmp/temp2.$$
 
-# Soprt by date
-sort -nk1 -t\t /tmp/temp2.$$ >/tmp/temp3.txt
+# Sort by date
+sort -nk1 -t\t /tmp/temp2.$$ >/tmp/temp3.$$
 
 # Fix header
-cat /tmp/temp3.txt | sed '2s/^[^\t]*\t\t//' | sed '1{N;s/\n//}' >/var/www/html/temp2.txt
+cat /tmp/temp3.$$ | sed '2s/^[^\t]*\t\t//' | sed '1{N;s/\n//}' >/var/www/html/temp2.txt
+
+# Remove temp files
+rm -f /tmp/temp2.$$ /tmp/temp3.$$
